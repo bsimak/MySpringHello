f200ecff5c9a8082d50d46e0e04ca68daf0b745d
B.simak <b.simak@gmail.com>
2021-01-02 15:15:13 +0100

    Initial commit

---
+++ src/test/java/io/simak/MySpringHelloApplicationTests.java
@@ -0,0 +1,14 @@
+package io.simak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Ich teste");
+	}
+
+}
